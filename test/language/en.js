2eb019cb37d917647a998c005771f02d361c5341
ellisMing <j0989574647@gmail.com>
2016-01-26 14:09:15 +0800

    modify language test case #2

--- test/language/en.js
+++ test/language/en.js
@@ -2,7 +2,7 @@ var env = require('../../environment.js');
 
 describe('zh-TW version',function() {
   var languageSelect = element(by.id('select_value_label_0'));
-  var twOption = element(by.css('[value="tw"]'));
+  var twOption = element(by.css('[value="en"]'));
   var naviTitle = element(by.css('[class="ng-binding"]'));
   var title = element(by.css('[class="md-headline ng-binding ng-scope"]'));
 
@@ -14,7 +14,7 @@ describe('zh-TW version',function() {
     browser.get(env.web);
   });
 
-  it('select en',function() {
+  it('i18n en',function() {
     wait(2);
     languageSelect.click();
     twOption.click();
