d5da75fcff3ad31f336ce8a64d64ada0770e6889
ellisMing <j0989574647@gmail.com>
2016-01-26 15:33:53 +0800

    modify language test case #3

--- test/language/en.js
+++ test/language/en.js
@@ -4,7 +4,7 @@ describe('zh-TW version',function() {
   var languageSelect = element(by.id('select_value_label_0'));
   var twOption = element(by.css('[value="en"]'));
   var naviTitle = element(by.css('[class="ng-binding"]'));
-  var title = element(by.css('[class="md-headline ng-binding ng-scope"]'));
+  var title = element(by.css('[class="md-headline ng-binding"]'));
 
   function wait(s) {
     browser.sleep(1000 * s);
